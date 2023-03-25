c81ed2bc6e8e512836cca6c1fe24331dd9240fe2
Jasenko <121662342+jsnko@users.noreply.github.com>
2023-03-25 11:14:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let countEl = document.getElementById("count-el");
 let count = 0;
+let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
 
 function increment() {
